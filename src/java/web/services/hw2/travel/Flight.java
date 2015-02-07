f20acd2eb936eb27d62a5f57e46396978c8a5560
Saman Jahanbakhsh <samanj@kth.se>
2015-02-07 18:14:30 +0100

    add some data

---
+++ src/java/web/services/hw2/travel/Flight.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web.services.hw2.travel;
+
+/**
+ *
+ * @author syst3m
+ */
+public class Flight {
+    private String departure;
+    private String destination;
+    private String change;
+    private String date;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getChange() {
+        return change;
+    }
+
+    public void setChange(String change) {
+        this.change = change;
+    }
+    private int price;
+
+    public String getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(String departure) {
+        this.departure = departure;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    
+}
---
+++ src/java/web/services/hw2/travel/Ticket.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web.services.hw2.travel;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author syst3m
+ */
+public class Ticket {
+   private static List <Flight> flightList = new ArrayList<Flight>(); 
+ 
+
+   public Ticket(){
+       Flight flight = new Flight();
+       Flight flight2 = new Flight();
+       Flight flight3 = new Flight();
+       Flight flight4 = new Flight();
+       Flight flight5 = new Flight();
+       
+       flight.setDeparture("stockholm");
+       flight.setStop("skövde");
+       flight.setDestination("göteborg");
+       flight.setPrice(400);
+       flight.setDate("2015-02-02");
+       flightList.add(flight);
+       
+       flight2.setDeparture("stockholm");
+       flight2.setStop("katrinholm");
+       flight2.setDestination("göteborg");
+       flight2.setPrice(300);
+       flight2.setDate("2015-02-12");
+       flightList.add(flight2);
+       
+        
+       flight3.setDeparture("stockholm");
+       flight3.setDestination("göteborg");
+       flight3.setStop("borås");
+       flight3.setPrice(300);
+       flight3.setDate("2015-02-15");
+       flightList.add(flight3);
+       
+       
+       flight4.setDeparture("danmark");
+       flight4.setDestination("bremen");
+       flight4.setStop("hamburg");
+       flight4.setPrice(1300);
+       flight4.setDate("2015-02-17");
+       flightList.add(flight4);
+       
+       flight5.setDeparture("danmark");
+       flight5.setDestination("bremen");
+       flight5.setStop("berlin");
+       flight5.setPrice(1300);
+       flight5.setDate("2015-02-17");
+       flightList.add(flight5);
+   }
+   
+   public List findTicket(String departure, String destination){
+       List ticket =  new ArrayList<Flight>();      
+       for(int i = 0; i < flightList.size(); ++i){           
+           if(flightList.get(i).getDeparture().equals(departure) && flightList.get(i).getDestination().equals(destination)){            
+               ticket.add(flightList.get(i));
+           }
+       }
+       return ticket;
+   }
+}
