75e77a305d90470299887f988f2c64c6f1578df3
alexgzb <alexgh@kth.se>
2015-02-09 18:44:10 +0100

    ForSam

--- src/java/web/services/hw2/travel/FlightCheckerWS.java
+++ src/java/web/services/hw2/travel/FlightCheckerWS.java
@@ -4,24 +4,26 @@
  * and open the template in the editor.
  */
 package web.services.hw2.travel;
-
+ 
 import java.util.ArrayList;
 import java.util.List;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.ejb.Stateless;
+import web.services.hw2.travel.model.Flight;
+import web.services.hw2.travel.model.Flights;
 import web.services.hw2.travel.model.Trip;
-
+ 
 /**
  *
  * @author Alex
  */
-@WebService(serviceName = "TravelAgencyWS")
+@WebService(serviceName = "FlightCheckerWS")
 @Stateless()
-public class TravelAgencyWS {
-
-
+public class FlightCheckerWS {
+ 
+ 
     /**
      * Web service operation
      *
@@ -30,12 +32,21 @@ public class TravelAgencyWS {
      * @return
      */
     @WebMethod(operationName = "checkFlight")
-    public List<Trip> checkFlight(@WebParam(name = "destination") String destination, @WebParam(name = "departure") String departure) {
-        List<Trip> trips = new ArrayList<>();
-
-        return trips;
+    public List<String> checkFlight(@WebParam(name = "departure") String departure,@WebParam(name = "destination") String destination)  {
+        List<Trip> trips = new ArrayList<Trip>();
+        List<String> stringlist = new ArrayList<String>();
+        Flights fl = new Flights();
+        trips = fl.findFligts(departure, destination);
+        for(int  i = 0 ; i < trips.size(); ++i){
+            String test = trips.get(i).toString();
+            System.out.println(" " + trips.toString());
+            stringlist.add(test);
+        }
+        
+       // System.out.println("teeest " + trips.get(0).toString());
+        return stringlist;
     }
-
+ 
     /**
      *
      * @param trip
